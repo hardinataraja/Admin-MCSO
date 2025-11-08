834e6571db84268bdc96c8a544fe8ace7e0dbd79
hardinataraja <hardinataraja@gmail.com>
2025-11-08 17:44:45 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,12 @@
+// config.js - RajaOrder Admin
+// Konfigurasi Firebase untuk panel admin (akses penuh Firestore)
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyC7VFvAsqRjiYinzfUfwabqHVvMWsvVhFo",
+  authDomain: "raja-kudapan.firebaseapp.com",
+  projectId: "raja-kudapan",
+  storageBucket: "raja-kudapan.firebasestorage.app",
+  messagingSenderId: "61175543723",
+  appId: "1:61175543723:web:57d4a4f64480cb7f4344ee",
+  measurementId: "G-ZGFTZER9RJ"
+};
